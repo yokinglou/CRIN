5a1d08e89b8d16e70424bb8acee53d64ec67d5e1
Yoking-Mac <865227010@qq.com>
2023-05-11 16:56:11 +0800

    add CRF

--- models/modules.py
+++ models/modules.py
@@ -20,6 +20,7 @@ class CRF(nn.Module):
         xyz = xyz.permute(0,2,1)
         normal = normal.permute(0,2,1)
         
+        # PCRF 1
         z_axis = torch.Tensor([0,0,1]).to(device).view(1,1,1,3).repeat(B,N,1,1)
         w1_axis = xyz.unsqueeze(2)
         w1_axis = w1_axis / torch.clamp(w1_axis.norm(dim=-1, keepdim=True), min=1e-5)
@@ -29,6 +30,7 @@ class CRF(nn.Module):
         b1 = torch.cat([u1_axis, v1_axis, w1_axis], dim=2)
         b1 = b1.transpose(2, 3)
 
+        # PCRF 2
         w2_axis = (normal.unsqueeze(2) @ b1)
         w2_axis = w2_axis / torch.clamp(w2_axis.norm(dim=-1, keepdim=True), min=1e-5)
         u2_axis = torch.cross(z_axis, w2_axis, dim=-1)
